4408c8bd5ac29207c4306d0df1d0ab6ac0aebbd0
davidochoa.dev <davidochoa.dev@gmail.com>
2024-05-12 20:11:54 +0000

    Feat: General edits on page.tsx of QR Code Comp

--- app/qr-code-component/page.tsx
+++ app/qr-code-component/page.tsx
@@ -1,17 +1,21 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { FaLinkedin, FaHouse, FaSquareGithub } from "react-icons/fa6";
 
 
 export default function Page() {
    return (
       <main className="bg-[#D5E1EF] w-screen h-screen flex items-center justify-center">
-         <div className="absolute bottom-0 left-1/2 mb-2 -translate-x-1/2 bg-[#181f2b] p-4 rounded-full opacity-70">
+         <div className="absolute bottom-0 left-1/2 mb-2 -translate-x-1/2 bg-[#181f2b] p-4 rounded-full opacity-70 flex flex-row gap-4">
             <Link href="/">
-               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-                  <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
-                  <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
-               </svg>
+               <FaHouse />
             </Link>
+            <a href="https://www.linkedin.com/in/davidochoadev/" target='_blank' rel='noreferrer'>
+               <FaLinkedin />
+            </a>
+            <a href="https://github.com/davidochoadev/frontend-mentor-projects/tree/main/app/qr-code-component" target='_blank' rel='noreferrer'>
+               <FaSquareGithub />
+            </a>
          </div>
 
          <div className="bg-white w-[320px] h-[497px] rounded-[20px] drop-shadow-xl flex flex-col items-center mx-[28px] p-[16px]">
@@ -22,11 +26,11 @@ export default function Page() {
                alt="QR Code Front-end Mentor"
                className="bg-blue-500 w-[288px] h-[288px] mx-[16px] mb-[16px] rounded-[10px]"
             />
-            <div className='mx-[16px] mt-[8px]'>
-               <h1 className=' font-bold text-[#1F314F] text-center text-base max-h-[56px] max-w-[288px] leading-tight'>
+            <div className='mt-[8px] flex flex-col items-center justify-center'>
+               <h1 className=' font-bold text-[#1F314F] text-center text-[22px] max-h-[56px] max-w-[288px] leading-tight'>
                   Improve your front-end skills by building projects
                </h1>
-               <p className='text-[0.70rem] text-[#7D889E] text-center mt-[16px] leading-snug max-w-[256px]'>
+               <p className='text-[15px] text-[#7D889E] text-center mt-[16px] leading-snug max-w-[256px]'>
                   Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
                </p>
             </div>
